39ee3d63cf0fee7b4a1f36752dd8470b504c0f4e
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-27 23:38:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,11 +18,11 @@ function whileLoop(n) {
 
 function maybeTrue() {
   return Math.random() >= 0.5
+}
 
 function doWhileLoop(array) {
   do {
     array.pop()
   } while (array.length > 0 && maybeTrue())
   return array 
-}
 }
